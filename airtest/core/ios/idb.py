979e2e7d53e2aec8bc430fcf922f7ff82c2cf6fe
Mitchell LeLiever <lelieverm@gmail.com>
2020-05-18 14:13:09 +0200

    Revert

--- airtest/core/ios/idb.py
+++ airtest/core/ios/idb.py
@@ -72,7 +72,7 @@ class IDB(object):
             process
 
         """
-        return self._cmd('launch', package)
+        return self._cmd('launch', package, '-f/--foreground-if-running')
 
     def stop_app(self, package):
         """
